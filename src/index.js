8ab69f9f3e7072d2081692552fdcf8bc338673d2
EverSorto <evercuentatres@gmail.com>
2020-12-28 13:55:40 -0600

    Se creo la primara pagina

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Badge from './components/badge'
 import 'bootstrap/dist/css/bootstrap.css'
 import './global.css'
+
+import BadgeNew from './pages/BadgeNew'
+
+
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(<Badge firstName="Ever" lastName="Sorto" aspiration="FullstackDevelop"/>, container);
+ReactDOM.render(<BadgeNew />, container);
---
+++ src/pages/BadgeNew.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import "./styles/BadgeNew.css";
+import Navbar from "../components/Navbar";
+import header from "../images/badge-header.svg";
+import Badge from '../components/Badge'
+class BadgeNew extends React.Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div className="badgeNew__hero">
+          <img className="img-fluid" src={header} alt="logo"></img>
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <Badge
+                firstName="Ever"
+                lastName="Sorto"
+                twitter="EverSorto"
+                inspiraion="fullStack developer"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default BadgeNew;
