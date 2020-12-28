f10e1b0f4a19da71a5380ec1e76f010af224f9ab
EverSorto <evercuentatres@gmail.com>
2020-12-28 11:14:11 -0600

    Se agrego el Css a la clase Badges

--- src/components/badge.js
+++ src/components/badge.js
@@ -1,25 +1,29 @@
 import React from 'react'
-import confLogo from '../images/pan.png'
+import confLogo from '../images/badge-header.svg'
+import './styles/Badge.css'
 
- class Badge extends React.Component{
-     render(){
-         return (
-             <div>
-                 <div>
-                     <img src={confLogo} alt="Logo de la conferencia"/>
-                 </div>
-                 <div>
-                     <img src="" alt="avatar" />
-                     <h1> Ever<br/> Sorto</h1>
-                 </div>
-                 <div>
-                     <p>FullStack developer</p>
-                     <p>#Panaderia Sorto</p>
-                     <p>especialista en seguridad informatica</p>
-                 </div>
-             </div>
-         )
-     }
- }
+class Badge extends React.Component {
+  render() {
+    return (
+      <div className="Badge">
+        <div className="Badge__header">
+          <img src={confLogo} alt="Logo de la conferencia" />
+        </div>
+        <div className="Badge__section-name">
+          <img className="Badge__avatar" src="" alt="avatar" />
+          <h1> Ever<br /> Sorto</h1>
+        </div>
+        <div className="Badge__section-info">
+          <p>FullStack developer</p>
+          <p>#Panaderia Sorto</p>
+        </div>
 
- export default Badge;
+        <div className="Badge__footer">
+
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Badge;
